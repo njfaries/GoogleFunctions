1c51883d4404417da795a180966f83299a4f8109
Nathaniel <njfaries@gmail.com>
2021-03-17 23:40:52 -0400

    Added suite test

---
+++ go/go_suite_test.go
@@ -0,0 +1,13 @@
+package p_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestGo(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Go Suite")
+}
