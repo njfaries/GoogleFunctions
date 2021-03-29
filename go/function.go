ba14aa6c5d13137646a15375979a0bda220ed4a7
Nathaniel <njfaries@gmail.com>
2021-03-29 19:29:20 -0400

    add ftp package

--- go/function.go
+++ go/function.go
@@ -3,9 +3,6 @@ package p
 
 import (
 	"encoding/json"
-	"fmt"
-	"html"
-	"io"
 	"log"
 	"net/http"
 )
@@ -35,32 +32,7 @@ func Decode(w http.ResponseWriter, r *http.Request) {
 	log.Print(url)
 }
 
-// HelloWorld prints the JSON encoded "message" field in the body
-// of the request or "Hello, World!" if there isn't one.
-func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	var d struct {
-		Message string `json:"message"`
-	}
-
-	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-		switch err {
-		case io.EOF:
-			fmt.Fprint(w, "Hi world!")
-			return
-		default:
-			log.Printf("json.NewDecoder: %v", err)
-			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-			return
-		}
-	}
-
-	if d.Message == "" {
-		fmt.Fprint(w, "Hello World!")
-		return
-	}
-	fmt.Fprint(w, html.EscapeString(d.Message))
-}
-
+//extract download_direct url
 func ExtractUrl(request Hook) string {
 	return request.LinkList.Url.Url
 }
--- go/go.mod
+++ go/go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	cloud.google.com/go/storage v1.14.0 // indirect
+	github.com/jlaffaye/ftp v0.0.0-20210307004419-5d4190119067 // indirect
 	github.com/onsi/ginkgo v1.15.2 // indirect
 	github.com/onsi/gomega v1.11.0 // indirect
 )
