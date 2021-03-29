967ea7e9d72a57cffe1f7d7a877b99057be6ec54
Nathaniel <njfaries@gmail.com>
2021-03-29 19:55:46 -0400

    Adds using env variables to access bucket list for testing

--- go/function.go
+++ go/function.go
@@ -5,6 +5,9 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/minio/minio-go"
 )
 
 type Href struct {
@@ -21,6 +24,24 @@ type Hook struct {
 }
 
 func Decode(w http.ResponseWriter, r *http.Request) {
+	accessKey := os.Getenv("accessKey")
+	secretKey := os.Getenv("secret")
+	endpoint := "nyc3.digitaloceanspaces.com"
+	client, err := minio.New(endpoint, accessKey, secretKey, true)
+	if err != nil {
+		log.Printf("error creating client: %v", err)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+	}
+
+	spaces, err := client.ListBuckets()
+	if err != nil {
+		log.Printf("error fetching buckets: %v", err)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+	}
+
+	for _, space := range spaces {
+		log.Printf("Space: %v", space.Name)
+	}
 	hook := Hook{}
 	if err := json.NewDecoder(r.Body).Decode(&hook); err != nil {
 		log.Printf("error occured: %v", err)
--- go/go.mod
+++ go/go.mod
@@ -4,7 +4,14 @@ go 1.16
 
 require (
 	cloud.google.com/go/storage v1.14.0 // indirect
+	github.com/go-ini/ini v1.62.0 // indirect
 	github.com/jlaffaye/ftp v0.0.0-20210307004419-5d4190119067 // indirect
+	github.com/minio/minio-go v6.0.14+incompatible // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/onsi/ginkgo v1.15.2 // indirect
 	github.com/onsi/gomega v1.11.0 // indirect
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
+	golang.org/x/net v0.0.0-20210329181859-df645c7b52b1 // indirect
+	golang.org/x/sys v0.0.0-20210326220804-49726bf1d181 // indirect
+	golang.org/x/text v0.3.5 // indirect
 )
