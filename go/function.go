e4115546afd940584fa5250491a491a76853305b
Nathaniel <njfaries@gmail.com>
2021-04-04 19:51:50 -0400

    experimental uploading of zip contents

--- go/function.go
+++ go/function.go
@@ -2,11 +2,15 @@
 package p
 
 import (
+	"archive/zip"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/minio/minio-go"
 )
@@ -47,7 +51,21 @@ func Decode(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 	}
 
+	files, err := Unzip("/tmp/build.zip", "/tmp/build")
+	if err != nil {
+		log.Printf("error occurde while unzipping: %v", err)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+	}
+
 	opts := minio.PutObjectOptions{}
+	for _, f := range files {
+		_, err := client.FPutObject("deleptualspace", "final-verdict-cicd-test/build/"+f, f, opts)
+		if err != nil {
+			log.Printf("error occured while uploading: %v", err)
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		}
+
+	}
 	uploadInfo, err := client.FPutObject("deleptualspace", "final-verdict-cicd-test/build", "/tmp/build.zip", opts)
 	if err != nil {
 		log.Printf("error occured while uploading: %v", err)
@@ -81,4 +99,62 @@ func Download(url string) error {
 	}
 
 	return nil
+}
+
+// Unzip will decompress a zip archive, moving all files and folders
+// within the zip file (parameter 1) to an output directory (parameter 2).
+func Unzip(src string, dest string) ([]string, error) {
+
+	var filenames []string
+
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return filenames, err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+
+		// Store filename/path for returning and using later on
+		fpath := filepath.Join(dest, f.Name)
+
+		// Check for ZipSlip. More Info: http://bit.ly/2MsjAWE
+		if !strings.HasPrefix(fpath, filepath.Clean(dest)+string(os.PathSeparator)) {
+			return filenames, fmt.Errorf("%s: illegal file path", fpath)
+		}
+
+		filenames = append(filenames, fpath)
+
+		if f.FileInfo().IsDir() {
+			// Make Folder
+			os.MkdirAll(fpath, os.ModePerm)
+			continue
+		}
+
+		// Make File
+		if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+			return filenames, err
+		}
+
+		outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			return filenames, err
+		}
+
+		rc, err := f.Open()
+		if err != nil {
+			return filenames, err
+		}
+
+		_, err = io.Copy(outFile, rc)
+
+		// Close the file without defer to close before next iteration of loop
+		outFile.Close()
+		rc.Close()
+
+		if err != nil {
+			return filenames, err
+		}
+	}
+	return filenames, nil
 }
