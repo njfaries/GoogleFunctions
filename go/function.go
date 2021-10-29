adaa18395244268ca28460e926fc5681562d1ecd
Nathaniel <njfaries@gmail.com>
2021-10-29 15:12:30 -0400

    fixes content encoding i hope

--- go/function.go
+++ go/function.go
@@ -162,8 +162,8 @@ func Download(url string, isAssets bool) error {
 }
 
 func Upload(files []string, src string, dest string, space string, client *minio.Client) error {
-	userMetaData := map[string]string{"x-amz-acl": "public-read", "content-type": "application/xml", "content-encoding": "gzip"}
-	opts := minio.PutObjectOptions{UserMetadata: userMetaData}
+	userMetaData := map[string]string{"x-amz-acl": "public-read"}
+	opts := minio.PutObjectOptions{UserMetadata: userMetaData, ContentType: "application/xml", ContentEncoding: "gzip"}
 	for _, f := range files {
 		log.Printf("Uploading file %s to %s", f, dest)
 		trimmedFilePath := strings.ReplaceAll(f, src, "")
